47b6e956fffc539528494c698e5f025f97ab2cbc
AlexDudarev <dudarev.av@gmail.com>
2015-09-23 18:50:41 +0300

    new layout

--- app/models/category.js
+++ app/models/category.js
@@ -3,6 +3,5 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   title: DS.attr('string'),
   description: DS.attr('string'),
-  tags: DS.attr('string'),
-  category: DS.belongsTo('category')
+  memories: DS.belongsTo('memo')
 });
--- tests/integration/components/memory-details-test.js
+++ tests/integration/components/memory-details-test.js
@@ -1,7 +1,7 @@
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
 
-moduleForComponent('memory-input', 'Integration | Component | memory input', {
+moduleForComponent('memory-details', 'Integration | Component | memory details', {
   integration: true
 });
 
@@ -11,15 +11,15 @@ test('it renders', function(assert) {
   // Set any properties with this.set('myProperty', 'value');
   // Handle any actions with this.on('myAction', function(val) { ... });
 
-  this.render(hbs`{{memory-input}}`);
+  this.render(hbs`{{memory-details}}`);
 
   assert.equal(this.$().text().trim(), '');
 
   // Template block usage:
   this.render(hbs`
-    {{#memory-input}}
+    {{#memory-details}}
       template block text
-    {{/memory-input}}
+    {{/memory-details}}
   `);
 
   assert.equal(this.$().text().trim(), 'template block text');
